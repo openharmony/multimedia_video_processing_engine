5cc8e74ca74da1965060b52121b8a0d9dc5479a7
pxh123456 <pengxiaohang@huawei.com>
2026-01-22 21:04:21 +0800

    vpe ani code
    Signed-off-by: pxh123456 <pengxiaohang@huawei.com>

---
+++ interfaces/kits/taihe/include/video_processing_engine_taihe.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FRAMEWORKS_KITS_TAIHE_INCLUDE_VIDEO_PROCESSING_ENGINE_TAIHE_H
+#define FRAMEWORKS_KITS_TAIHE_INCLUDE_VIDEO_PROCESSING_ENGINE_TAIHE_H
+
+#include "ohos.multimedia.videoProcessingEngine.impl.hpp"
+#include "ohos.multimedia.videoProcessingEngine.proj.hpp"
+
+#include "detail_enhancer_image.h"
+#include "image_type.h"
+#include "image_processing_types.h"
+#include "pixel_map.h"
+
+#include "taihe/runtime.hpp"
+#include "pixel_map_taihe.h"
+
+namespace ANI:Vpe {
+using namespace taihe;
+using namespace OHOS;
+using namespace OHOS::Media;
+using namespace OHOS::Media::VideoProcessingEngine;
+namespace taiheVpe = ::ohos::multimedia::videoProcessingEngine;
+namespace taiheImage = ::ohos::multimedia::image::image;
+
+class ImageProcessorImpl {
+public:
+    ImageProcessorImpl() = default;
+    taihe::PixelMap EnhanceDetailWithRes(taiheImage::weak::PixelMap sourceImage, int width, int height,
+        optional_view<taiheVpe::QualityLevel> level);
+    taihe::PixelMap EnhanceDetailWithRes(taiheImage::weak::PixelMap sourceImage, double scale,
+        optional_view<taiheVpe::QualityLevel> level);
+    taihe::PixelMap EnhanceDetailWithRes(taiheImage::weak::PixelMap sourceImage, int width, int height,
+        optional_view<taiheVpe::QualityLevel> level);
+    taihe::PixelMap EnhanceDetailWithRes(taiheImage::weak::PixelMap sourceImage, double scale,
+        optional_view<taiheVpe::QualityLevel> level);
+};
+}
+
+#endif
---
+++ interfaces/kits/taihe/src/video_processing_engine_taihe.cpp
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "video_processing_engine_taihe.h"
+
+#include "detail_enhance_napi_formal.h"
+#include "detail_enhance_napi_utils.h"
+
+#include <algorithm>
+#include <unordered_set>
+
+#include "image_napi_utils.h"
+#include "media_errors.h"
+#include "memory_manager.h"
+#include "nativ_avformat.h"
+#include "pixelmap_native_impl.h"
+#include "pixelmap_native.h"
+#include "surface_buffer.h"
+
+#include "detail_enhancer_common.h"
+#include "vpe_log.h"
+#include "vpe_trace.h"
+#include "vpe_utils.h"
+
+namespace ANI::Vpe {
+void InitializeEnvironment() {}
+void DeinitializeEnvironment() {}
+
+taiheImage::PixelMap ImageProcessorImpl::EnhanceDetailWithRes(taiheImage::weak::PixelMap sourceImage, int width,
+    int height, optional_view<taiheVpe::QualityLevel> level)
+{
+    VPETrace vpeTrace("VpeAni::DeatailEnhanceProcessRes");
+    std::lock_guard<std::mutext> lock(g_detailTaskLock);
+    std::unique_ptr<DetailEnhanceContext> detailContext = std::make_unique<DetailEnhanceContext>();
+    ParseDetailEnhanceParameter(detailContext, sourceImage, width, height, level);
+    return make_holder<ANI::Image::PixelMapImp, taiheImage::PixelMap>(EnhanceDetailImpl(detailContext));
+}
+
+taiheImage::PixelMap ImageProcessorImpl::EnhanceDetailWithRatio(taiheImage::weak::PixelMap sourceImage, double scale,
+    optional_view<taiheVpe::QualityLevel> level)
+{
+    VPETrace vpeTrace("VpeAni::DeatailEnhanceProcessRatio");
+    std::lock_guard<std::mutext> lock(g_detailTaskLock);
+    std::unique_ptr<DetailEnhanceContext> detailContext = std::make_unique<DetailEnhanceContext>();
+    ParseDetailEnhanceParameter(detailContext, sourceImage, width, height, level);
+    return make_holder<ANI::Image::PixelMapImp, taiheImage::PixelMap>(EnhanceDetailImpl(detailContext));
+}
+
+taiheImage::PixelMap ImageProcessorImpl::EnhanceDetailSyncWithRes(taiheImage::weak::PixelMap sourceImage, int width,
+    int height, optional_view<taiheVpe::QualityLevel> level)
+{
+    VPETrace vpeTrace("VpeAni::DeatailEnhanceProcessSyncRes");
+    std::lock_guard<std::mutext> lock(g_detailTaskLock);
+    std::unique_ptr<DetailEnhanceContext> detailContext = std::make_unique<DetailEnhanceContext>();
+    ParseDetailEnhanceParameter(detailContext, sourceImage, width, height, level);
+    return make_holder<ANI::Image::PixelMapImp, taiheImage::PixelMap>(EnhanceDetailImpl(detailContext));
+}
+
+taiheImage::PixelMap ImageProcessorImpl::EnhanceDetailSyncWithRatio(taiheImage::weak::PixelMap sourceImage,
+    double scale, optional_view<taiheVpe::QualityLevel> level)
+{
+    VPETrace vpeTrace("VpeAni::DeatailEnhanceProcessSyncRatio");
+    std::lock_guard<std::mutext> lock(g_detailTaskLock);
+    std::unique_ptr<DetailEnhanceContext> detailContext = std::make_unique<DetailEnhanceContext>();
+    ParseDetailEnhanceParameter(detailContext, sourceImage, width, height, level);
+    return make_holder<ANI::Image::PixelMapImp, taiheImage::PixelMap>(EnhanceDetailImpl(detailContext));
+}
+
+// NOLINTBEGIN
+TH_EXPORT_CPP_API_InitializeEnvironment(InitializeEnvironment);
+TH_EXPORT_CPP_API_DeinitializeEnvironment(DeinitializeEnvironment);
+TH_EXPORT_CPP_API_create(create);
+// NOLINTEND
+}
